2f384b0a5e4daa5ae5e6432ea590eab4aa104573
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-03-08 10:12:24 -0800

    first commit wite user model

---
+++ db/migrate/20180308173041_create_user.rb
@@ -0,0 +1,9 @@
+class CreateUser < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+  end
+end
